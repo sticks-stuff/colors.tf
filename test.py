9e5869181ee96e9dc6ccf974aa04a72ef17e7f90
Thestickman391 <stickman391@gmail.com>
2021-12-16 16:00:29 +1300

    displaying the colours in a reasonable way

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from typing import List
 import datamodel
 import os
 
@@ -6,41 +7,80 @@ print('<link rel="stylesheet" href="style.css">')
 for filename in os.listdir('particles'):
     particle = datamodel.load('particles/' + filename)
     # print(filename)
+    print('<div>')
     print('<h1>' + filename + '</h1>')
+    particles = {}
     for ele in particle.find_elements(elemtype="DmeParticleSystemDefinition"):
         # print(ele.name)
-        print('<h2>' + ele.name + '</h2>')
+        thisParticle = ''
+        if 'red' in ele.name:
+            thisParticle += '<div class="red-particle">\n'
+            thisParticle += '<h2>' + ele.name + '</h2>\n'
+        else:
+            thisParticle += '<div class="blue-particle">\n'
+            thisParticle += '<h2>' + ele.name + '</h2>\n'
+            
+        # print('<div>')
+        # print('<h2>' + ele.name + '</h2>')
         for operator in ele.get('operators'):
             if(operator.name == "Color Fade"):
+                thisParticle += '<div>'
                 # print("color_fade")
                 # print("operator.get("color_fade")")
-                print('<h3> color_fade </h3>')
-                print("<div style=\"background-color: rgb(", end = '')
-                print(operator.get("color_fade")[0], end = '')
-                print(", ", end = '')
-                print(operator.get("color_fade")[1], end = '')
-                print(", ", end = '')
-                print(operator.get("color_fade")[2], end = '')
-                print(");\"></div>")
+                thisParticle += '<h3> color_fade </h3>\n'
+                thisParticle += "<div class='colour-display' style=\"background-color: rgb("
+                thisParticle += str(operator.get("color_fade")[0])
+                thisParticle += ", "
+                thisParticle += str(operator.get("color_fade")[1])
+                thisParticle += ", "
+                thisParticle += str(operator.get("color_fade")[2])
+                thisParticle += ");\"></div>\n"
+                thisParticle += '</div>\n'
         for initializer in ele.get('initializers'):
             if(initializer.name == "Color Random"):
-                # print("color1")
-                print('<h3> color1 </h3>')
-                # print(initializer.get("color1"))
-                print("<div style=\"background-color: rgb(", end = '')
-                print(initializer.get("color1")[0], end = '')
-                print(", ", end = '')
-                print(initializer.get("color1")[1], end = '')
-                print(", ", end = '')
-                print(initializer.get("color1")[2], end = '')
-                print(");\"></div>")
-                # print("color2")
-                print('<h3> color2 </h3>')
-                # print(initializer.get("color1"))
-                print("<div style=\"background-color: rgb(", end = '')
-                print(initializer.get("color2")[0], end = '')
-                print(", ", end = '')
-                print(initializer.get("color2")[1], end = '')
-                print(", ", end = '')
-                print(initializer.get("color2")[2], end = '')
-                print(");\"></div>")
+                thisParticle += '<div>\n'
+                # thisParticle += "color1")
+                thisParticle += '<h3> color1 </h3>\n'
+                # thisParticle += initializer.get("color1"))
+                thisParticle += "<div class='colour-display' style=\"background-color: rgb("
+                thisParticle += str(initializer.get("color1")[0])
+                thisParticle += ", "
+                thisParticle += str(initializer.get("color1")[1])
+                thisParticle += ", "
+                thisParticle += str(initializer.get("color1")[2])
+                thisParticle += ");\"></div>\n"
+                thisParticle += '</div>\n'
+                # thisParticle += "color2")
+                thisParticle += '<div>\n'
+                thisParticle += '<h3> color2 </h3>\n'
+                # thisParticle += initializer.get("color1"))
+                thisParticle += "<div class='colour-display' style=\"background-color: rgb("
+                thisParticle += str(initializer.get("color2")[0])
+                thisParticle += ", "
+                thisParticle += str(initializer.get("color2")[1])
+                thisParticle += ", "
+                thisParticle += str(initializer.get("color2")[2])
+                thisParticle += ");\"></div>\n"
+                thisParticle += '</div>\n'
+        thisParticle += '</div>\n'
+                
+        if 'red' in ele.name:
+            if ele.name.replace('red', 'team') not in particles:
+                particles[ele.name.replace('red', 'team')] = {}
+            particles[ele.name.replace('red', 'team')]['red'] = thisParticle
+        else:
+            if 'blu_' in ele.name or ele.name.endswith('_blu'):
+                if ele.name.replace('blu', 'team') not in particles:
+                    particles[ele.name.replace('blu', 'team')] = {}
+                particles[ele.name.replace('blu', 'team')]['blue'] = thisParticle
+            else:
+                if ele.name.replace('blue', 'team') not in particles:
+                    particles[ele.name.replace('blue', 'team')] = {}
+                particles[ele.name.replace('blue', 'team')]['blue'] = thisParticle
+    for key, value in particles.items():
+        print('<div class="both-class-colours">')
+        for key2, value2 in value.items():
+            print(value2)
+        print('</div>')
+    print('</div>')
+    
