e6fadf1f18a31382f3de293ed4621343083b82e8
Thestickman391 <stickman391@gmail.com>
2021-12-18 16:34:43 +1300

    json dump of particle colours

--- change-values.py
+++ change-values.py
@@ -1,50 +1,68 @@
 import datamodel
 import os
+import json
+
+data = {}
 
 for filename in os.listdir('particles'):
     particle = datamodel.load('particles/' + filename)
     print('\n')
     print(filename)
     particleFile = open('particles/' + filename, "r+b")
+    data[filename] = {}
     for ele in particle.find_elements(elemtype="DmeParticleSystemDefinition"):
         print('\n')
         print(ele.name)
         for operator in ele.get('operators'):
             if(operator.name == "Color Fade"):
-                # white = datamodel.Color([25, 25, 25, 25])
-                # print(type(operator['color_fade']))
-                # data = list([0, 4, 8, 9])
-                # operator['color_fade'] = datamodel.make_array([ [0.0, 0.0, 0.5, 0.5] ] , datamodel.Color)
+                if ele.name not in data[filename]:
+                        data[filename][ele.name] = {}
+                print("color_fade")
                 print(operator['color_fade'].offset)
                 particleFile.seek(operator['color_fade'].offset)
                 particleFile.write(bytes((255,255,255)))
+                
+                data[filename][ele.name]['color_fade'] = "255 255 255"
+                
                 print(operator.get("color_fade")[0], end = '')
                 print(", ", end = '')
                 print(operator.get("color_fade")[1], end = '')
                 print(", ", end = '')
-                print(operator.get("color_fade")[2], end = '')
+                print(operator.get("color_fade")[2])
+                
         for initializer in ele.get('initializers'):
             if(initializer.name == "Color Random"):
-                print("\ncolor1")
-                print(initializer.get("color1")[0], end = '')
-                print(", ", end = '')
-                print(initializer.get("color1")[1], end = '')
-                print(", ", end = '')
-                print(initializer.get("color1")[2], end = '')
+                if ele.name not in data[filename]:
+                    data[filename][ele.name] = {}
+                print("color1")
                 print(initializer['color1'].offset)
                 particleFile.seek(initializer['color1'].offset)
                 particleFile.write(bytes((255,255,255)))
                 
-                print("\ncolor2")
+                data[filename][ele.name]['color1'] = "255 255 255"
+
+                print(initializer.get("color1")[0], end = '')
+                print(", ", end = '')
+                print(initializer.get("color1")[1], end = '')
+                print(", ", end = '')
+                print(initializer.get("color1")[2])
+                
+                print("color2")
+                print(initializer['color2'].offset)
+                particleFile.seek(initializer['color2'].offset)
+                particleFile.write(bytes((255,255,255)))
+                
+                data[filename][ele.name]['color2'] = "255 255 255"
+
                 print(initializer.get("color2")[0], end = '')
                 print(", ", end = '')
                 print(initializer.get("color2")[1], end = '')
                 print(", ", end = '')
-                print(initializer.get("color2")[2], end = '')
+                print(initializer.get("color2")[2])
                 
-                print(initializer['color2'].offset)
-                particleFile.seek(initializer['color2'].offset)
-                particleFile.write(bytes((255,255,255)))
     particleFile.close()
     # particle.write('particles/' + filename, "binary", 2)
     # particle.write('particles/' + filename, "binary", 2)
+    
+with open('output.json', 'w') as outfile:
+    json.dump(data, outfile)
