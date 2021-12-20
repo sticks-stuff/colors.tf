4698aa8fc5432d791d2d8aa45337267785769e59
charlotte <charlottemackenzi@gmail.com>
2021-12-20 18:45:31 +1300

    ditto

--- json-to-color-patch.py
+++ json-to-color-patch.py
@@ -1,70 +1,58 @@
 import datamodel
-import os
-import json
 
-with open('output.json') as f:
-    data = json.load(f)
+def pathPCFWithJson(data):
+    for filename in data:
+        particle = datamodel.load('work/particles/' + filename)
+        # print('\n')
+        # print(filename)
+        particleFile = open('work/particles/' + filename, "r+b")
 
-# print(data)
-# for item in data:
-#     print(item)
-#     for item2 in data[item]:
-#         print(item2)
-#         for item3 in data[item][item2]:
-#             print(item3)
+        for ele in particle.find_elements(elemtype="DmeParticleSystemDefinition"):
+            if ele.name in data[filename]:
+                print('\n')
+                print(ele.name)
+                if 'color_fade' in data[filename][ele.name]:
+                    for operator in ele.get('operators'):
+                        if(operator.name == "Color Fade"):
+                            
+                            print("color_fade")
+                            print(operator['color_fade'].offset)
+                            particleFile.seek(operator['color_fade'].offset)
+                            for i in data[filename][ele.name]['color_fade']:
+                                particleFile.write(bytes((int(i),)))
+                            
+                            print(operator.get("color_fade")[0], end = '')
+                            print(", ", end = '')
+                            print(operator.get("color_fade")[1], end = '')
+                            print(", ", end = '')
+                            print(operator.get("color_fade")[2])
+                            
+                if 'color1' in data[filename][ele.name]:
+                    for initializer in ele.get('initializers'):
+                        if(initializer.name == "Color Random"):
 
-for filename in data:
-    particle = datamodel.load('particles/' + filename)
-    print('\n')
-    print(filename)
-    particleFile = open('particles/' + filename, "r+b")
-
-    for ele in particle.find_elements(elemtype="DmeParticleSystemDefinition"):
-        if ele.name in data[filename]:
-            print('\n')
-            print(ele.name)
-            if 'color_fade' in data[filename][ele.name]:
-                for operator in ele.get('operators'):
-                    if(operator.name == "Color Fade"):
-                        
-                        print("color_fade")
-                        print(operator['color_fade'].offset)
-                        particleFile.seek(operator['color_fade'].offset)
-                        for i in data[filename][ele.name]['color_fade']:
-                            particleFile.write(bytes((int(i),)))
-                        
-                        print(operator.get("color_fade")[0], end = '')
-                        print(", ", end = '')
-                        print(operator.get("color_fade")[1], end = '')
-                        print(", ", end = '')
-                        print(operator.get("color_fade")[2])
-                        
-            if 'color1' in data[filename][ele.name]:
-                for initializer in ele.get('initializers'):
-                    if(initializer.name == "Color Random"):
-
-                        print("color1")
-                        print(initializer['color1'].offset)
-                        particleFile.seek(initializer['color1'].offset)
-                        for i in data[filename][ele.name]['color1']:
-                            particleFile.write(bytes((int(i),)))
-                        
-                        print(initializer.get("color1")[0], end = '')
-                        print(", ", end = '')
-                        print(initializer.get("color1")[1], end = '')
-                        print(", ", end = '')
-                        print(initializer.get("color1")[2])
-                        
-                        print("color2")
-                        print(initializer['color2'].offset)
-                        particleFile.seek(initializer['color2'].offset)
-                        for i in data[filename][ele.name]['color2']:
-                            particleFile.write(bytes((int(i),)))
-                        
-                        print(initializer.get("color2")[0], end = '')
-                        print(", ", end = '')
-                        print(initializer.get("color2")[1], end = '')
-                        print(", ", end = '')
-                        print(initializer.get("color2")[2])
-                
-    particleFile.close()
+                            print("color1")
+                            print(initializer['color1'].offset)
+                            particleFile.seek(initializer['color1'].offset)
+                            for i in data[filename][ele.name]['color1']:
+                                particleFile.write(bytes((int(i),)))
+                            
+                            print(initializer.get("color1")[0], end = '')
+                            print(", ", end = '')
+                            print(initializer.get("color1")[1], end = '')
+                            print(", ", end = '')
+                            print(initializer.get("color1")[2])
+                            
+                            print("color2")
+                            print(initializer['color2'].offset)
+                            particleFile.seek(initializer['color2'].offset)
+                            for i in data[filename][ele.name]['color2']:
+                                particleFile.write(bytes((int(i),)))
+                            
+                            print(initializer.get("color2")[0], end = '')
+                            print(", ", end = '')
+                            print(initializer.get("color2")[1], end = '')
+                            print(", ", end = '')
+                            print(initializer.get("color2")[2])
+                    
+        particleFile.close()
