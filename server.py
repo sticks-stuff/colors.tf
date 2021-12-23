925f8965562d28ea1ef08b243583d36cccd339f0
charlotte <charlottemackenzi@gmail.com>
2021-12-23 22:37:38 +1300

    better logging

--- server.py
+++ server.py
@@ -1,13 +1,13 @@
 import json_to_color_patch
 import patch_vmts
-from flask import Flask, request
+from flask import Flask, request, send_file
 from flask_cors import CORS
 import shutil
 import time
 import vpk
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, expose_headers=["Content-Disposition"])
 
 @app.route('/generate', methods=['POST'])
 def generate():
@@ -18,19 +18,25 @@ def generate():
         shutil.copytree("materials/", "work/" + requestTime + "/materials/")
         shutil.copytree("template/particles", "work/" + requestTime + "/particles/")
 
-        print('Data Received: "{data}"'.format(data=data))
+        # print('Data Received: "{data}"'.format(data=data))
+        print("New request: " + requestTime);
         red_crit = data['material']['red_crit']['color']
         red_minicrit = data['material']['red_minicrit']['color']
         blue_crit = data['material']['blue_crit']['color']
         blue_minicrit = data['material']['blue_minicrit']['color']
         patch_vmts.patchVMTs(blue_crit, red_crit, red_minicrit, blue_minicrit, "work/" + requestTime + "/materials/")
+        print("Patched vmts!");
         
         del data['material'] #clean up the stuff thats only for vmfs
         json_to_color_patch.patchPCFWithJson(data, "work/" + requestTime + "/particles/")
+        print("Patched particles!");
+        
         newpak = vpk.new("work/" + requestTime)
         newpak.save("colors.tf_" + requestTime + ".vpk")
+        print("Packed files!");
 
-        return "Request Processed.\n"
+        path = "colors.tf_" + requestTime + ".vpk"
+        return send_file(path, as_attachment=True, attachment_filename=path)
 
 if __name__ == "__main__":
 #   app.run(port=3000)
