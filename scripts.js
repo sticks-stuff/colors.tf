3a0e68b8ea9b71b5215887a4b08b9e7314557136
charlotte <charlottemackenzi@gmail.com>
2021-12-29 14:43:21 +1300

    add collapsable shit

--- scripts.js
+++ scripts.js
@@ -247,3 +247,18 @@ function disableAlphaControl(picker) {
         this.source.value = this.color(r, g, b, 1);
     });
 }
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight){
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
